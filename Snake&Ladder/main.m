6c5da45c4aad533c0ec48dade0d9c45801f0ba65
ttjnguyen <ttjnguyen6@gmail.com>
2016-11-05 17:07:37 -0700

    Initial Commit

---
+++ Snake&Ladder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Snake&Ladder
+//
+//  Created by Jenny Nguyen on 2016-11-05.
+//  Copyright © 2016 LightHouseLabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
